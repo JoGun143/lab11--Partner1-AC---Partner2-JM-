15f864d279ffd53f1d00cd0e44501ba1e340671c
JoGun143 <mauldinjoey@gmail.com>
2025-11-14 09:19:27 -0500

    merged partner changes

--- calculator.py
+++ calculator.py
@@ -4,8 +4,31 @@ calculator.py
 
 One function per operation, in order.
 """
-# First example
-def add(a, b): 
-    pass
+#https://github.com/JoGun143/lab11--Partner1-AC---Partner2-JM-
+# Partner 1: <Ameerul Chowdhury>
+# Partner 2: <Joseph Mauldin>
 
+import math
+
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if b == 0:
+        raise ZeroDivisionError("Cannot divide by zero")
+    return a / b
+
+def log(a, b):
+    if a <= 0 or b <= 0 or b == 1:
+        raise ValueError("Invalid input for logarithm")
+    return math.log(a, b)
+
+def exp(a, b):
+    return a ** b
 
