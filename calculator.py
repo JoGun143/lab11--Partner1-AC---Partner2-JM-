c55eb918fb0006e908446ead61e9ead38647dee6
ameerc1 <ameerc207@gmail.com>
2025-11-18 17:57:49 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -3,9 +3,10 @@
 # Partner 2: <Joseph Mauldin>
 import math
 
+
 def square_root(a):
     if a < 0:
-        raise ValueError("Cannot take square root of a negative number.")
+        raise ValueError("Cannot take square root of negative number.")
     return math.sqrt(a)
 
 def hypotenuse(a, b):
@@ -14,30 +15,22 @@ def hypotenuse(a, b):
 def add(a, b):
     return a + b
 
-def subtract(a, b):
+def sub(a, b):
     return a - b
 
 def mul(a, b):
     return a * b
 
 def div(a, b):
-    if b == 0:
+    if a == 0:
         raise ZeroDivisionError("Cannot divide by zero.")
-    return a / b
+    return b / a
 
-def multiply(a, b):
-    return a * b
-
-def divide(a, b):
-    if b == 0:
-        raise ZeroDivisionError("Cannot divide by zero.")
-    return a / b
-
-def logarithm(a, b):
+def log(a, b):
     if a <= 0:
-        raise ValueError("Logarithm argument must be positive.")
+        raise ValueError("Argument must be positive.")
     if b <= 0 or b == 1:
-        raise ValueError("Invalid base for logarithm.")
+        raise ValueError("Invalid base.")
     return math.log(a, b)
 
 def exp(a, b):
