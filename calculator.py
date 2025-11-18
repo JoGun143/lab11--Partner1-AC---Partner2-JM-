184709db1cf31895ae6f6c180a33082e534db864
JoGun143 <mauldinjoey@gmail.com>
2025-11-18 15:53:21 -0500

    merged changes

--- calculator.py
+++ calculator.py
@@ -1,34 +1,36 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
-
-One function per operation, in order.
-"""
 #https://github.com/JoGun143/lab11--Partner1-AC---Partner2-JM-
 # Partner 1: <Ameerul Chowdhury>
 # Partner 2: <Joseph Mauldin>
-
 import math
+
+def square_root(a):
+    if a < 0:
+        raise ValueError("Cannot take square root of a negative number.")
+    return math.sqrt(a)
+
+def hypotenuse(a, b):
+    return math.hypot(a, b)
 
 def add(a, b):
     return a + b
 
-def sub(a, b):
+def subtract(a, b):
     return a - b
 
-def mul(a, b):
+def multiply(a, b):
     return a * b
 
-def div(a, b):
+def divide(a, b):
     if b == 0:
-        raise ZeroDivisionError("Cannot divide by zero")
+        raise ZeroDivisionError("Cannot divide by zero.")
     return a / b
 
-def log(a, b):
-    if a <= 0 or b <= 0 or b == 1:
-        raise ValueError("Invalid input for logarithm")
+def logarithm(a, b):
+    if a <= 0:
+        raise ValueError("Logarithm argument must be positive.")
+    if b <= 0 or b == 1:
+        raise ValueError("Invalid base for logarithm.")
     return math.log(a, b)
 
-def exp(a, b):
+def exponent(a, b):
     return a ** b
-
