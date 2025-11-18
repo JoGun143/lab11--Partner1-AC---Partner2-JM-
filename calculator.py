3a3a237b7427091a64c00af71013a1c589718324
JoGun143 <134387396+JoGun143@users.noreply.github.com>
2025-11-18 18:10:39 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -26,11 +26,11 @@ def mul(a, b):
 def multiply(a, b):
     return a * b
 def div(a, b):
-    if b == 0:
+    if a == 0 or b == 0:
         raise ZeroDivisionError("Cannot divide by zero.")
     return a / b
 def divide(a, b):
-    if b == 0:
+    if a == 0 or b == 0:
         raise ZeroDivisionError("Cannot divide by zero.")
     return a / b
 def log(a, b):
